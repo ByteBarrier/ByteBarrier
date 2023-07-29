7e126168ba97712333d14c8cabf1d73b1c937abd
Parallex <76573881+Alux6@users.noreply.github.com>
2023-07-29 19:50:42 +0200

    Terminada descompresión
    Falta crear una clase para la descompresiónç

--- includes/Arbol.h
+++ includes/Arbol.h
@@ -5,6 +5,7 @@
 #include "../src/Node.cpp"
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -73,6 +74,7 @@ class Arbol {
     Node raiz;
 
   public:
+    Arbol(const map<char,string> &huffman);
     inline Arbol() {}
     inline Node getRaiz() { return this->raiz; }
     friend class LinkedNode;
